755f10dd152aac266284cb3f0d91fc4842d73c75
Denis <omelched@gmail.com>
2025-07-07 17:20:08 +0300

    Задание 5

--- apps/temperature-api/temperature_api/__init__.py
+++ apps/temperature-api/temperature_api/__init__.py
@@ -6,18 +6,41 @@ from starlette.responses import JSONResponse
 
 app = FastAPI()
 
+def formatted_now():
+    ftime = datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%dT%H:%M:%S%z")
+    if ftime.endswith('+0000'):
+        ftime = ftime[:-5] + 'Z'
+
+    return ftime
 
 @app.get("/temperature/")
-async def create_item(location: str):
+async def get_by_location(location: str):
     temp = random.uniform(-50, 50)
     return JSONResponse(
         {
             "Value": temp,
             "Unit": "°C",
-            "Timestamp": datetime.datetime.now().timestamp(),
+            "Timestamp": formatted_now(),
             "Location": location,
             "Status": "on",
             "SensorId": "00000000-0000-0000-0000-000000000001",
+            "SensorType": "temperature",
+            "Description": "temparature sensor",
+        }
+    )
+
+
+@app.get("/temperature/{id}")
+async def get_by_id(id: int):
+    temp = random.uniform(-50, 50)
+    return JSONResponse(
+        {
+            "Value": temp,
+            "Unit": "°C",
+            "Timestamp": formatted_now(),
+            "Location": "...",
+            "Status": "on",
+            "SensorId": id,
             "SensorType": "temperature",
             "Description": "temparature sensor",
         }
